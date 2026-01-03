aa9776c1b0bf6065145ab571b702e3070db668c9
Torfinhell <you@example.com>
2026-01-03 17:13:38 +0300

    for kaggle

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ from pathlib import Path
 
 #PARAMETRS
 class Config:
-    WINDOW_SIZE=(44, 44)
+    WINDOW_SIZE=(40, 40)
     LAST_LINEAR_SIZE=1000
-    BATCH_SIZE=1024
+    BATCH_SIZE=2048
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     MEAN=None
     STD=None
     ROTATE_LIMIT=45
     SCALE_LIMIT=0.1
     SHIFT_LIMIT=0.1
-    LEARNING_RATE=6e-3
+    LEARNING_RATE=8e-3
     ACCUM_STEP=1
     NUM_WORKERS=os.cpu_count() or 1
     LOG_STEP=5
-    NUM_EPOCHS=1000
+    NUM_EPOCHS=1500
     LOSS="CE"
     MODEL="RESNET18"
     NUM_CLASSES=200
