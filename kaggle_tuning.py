7183038ab9457ecf29560b3f84161058c2154bd5
Torfinhell <nikita2school@icloud.com>
2026-01-20 23:01:25 +0300

    resnet_18_arcface_m_10

--- kaggle_tuning.py
+++ kaggle_tuning.py
@@ -1,123 +1,31 @@
-# import optuna
-# import torch
-# from pathlib import Path
-# from main import train_detector 
-# import argparse
-# from modules.config import Config
-# TRAIN_IMAGES = "/kaggle/input/bhw1/trainval"
-# LABELS_CSV = "/kaggle/input/bhw1/labels.csv"
-# SAVE_DIR = "optuna_models"
-# WANDB_TOKEN=None
-# Path(SAVE_DIR).mkdir(exist_ok=True)
-# # WANDB_TOKEN=None
-# # TRAIN_IMAGES = "bhw-1-dl-2025-2026/bhw1/trainval"
-# # LABELS_CSV = "bhw-1-dl-2025-2026/bhw1/labels.csv"
-# # SAVE_DIR = "optuna_models"
-# def objective(trial: optuna.Trial):
-#     config = Config()
-#     config.LEARNING_RATE = trial.suggest_float(
-#         "learning_rate", 1e-5,1e-2, log=True
-#     )
-#     config.WEIGHT_DECAY = trial.suggest_float(
-#         "weight_decay", 1e-5,1e-2, log=True
-#     )
-#     config.MAGNITUDE=trial.suggest_int(
-#         "magnitude", 10, 70, step=10
-#     )
-#     # size=trial.suggest_int(
-#     #     "window_size", 20, 60, step=4
-#     # )
-#     # config.WINDOW_SIZE=(size, size)
-#     config.MODEL = trial.suggest_categorical(
-#         "model",
-#         ["RESNET18", "RESNET34", "RESNET50"]
-#     )
-#     # config.MODEL="RESNET18"
-#     # config.NUM_BLOCKS=trial.suggest_int(
-#     #     "num_blocks",1, 5, step=1
-#     # )
-#     config.LAST_LINEAR_SIZE = trial.suggest_int(
-#         "last_linear_size", 200, 10000, step=200
-#     )
-#     config.MARGIN_ARCFACE = trial.suggest_float(
-#         "margin_arcface", 0.0,0.5
-#     )
-
-#     config.SCALE_ARCFACE = trial.suggest_int(
-#         "scale_arcface", 3, 30, step=4
-#     )
-    
-#     config.BATCH_SIZE = 2048
-#     config.SWA_START=None
-#     # config.LOSS = "ArcMargin"
-#     # config.OPTIMIZER="SGD"
-#     config.NUM_EPOCHS = 450
-#     config.STOP_EPOCH=10           
-#     config.WANDB_TOKEN = WANDB_TOKEN
-#     config.RUN_NAME=f"model_{config.MODEL}_arcface_m_{config.MARGIN_ARCFACE:.2f}"
-#     # config.RUN_NAME=f"resnet50_ablation_lr_{config.LEARNING_RATE:.6f}_wd_{config.WEIGHT_DECAY:.6f}_m_{config.MAGNITUDE}"
-#     config.WANDB_PROJECT="Tuning Arcface"
-#     config.TRAININ_DIR=TRAIN_IMAGES
-#     try:
-#         best_acc = train_detector(
-#             labels_csv=LABELS_CSV,
-#             images_path=TRAIN_IMAGES,
-#             config=config,
-#             save_model_path=None  
-#         )
-#     except RuntimeError as e:
-#         print("Runtime error:", e)
-#         return 0.0
-
-#     return best_acc
-
-
-# if __name__ == "__main__":
-
-#     study = optuna.create_study(
-#         direction="maximize",
-#         study_name="arcface_model_search"
-#     )
-#     parser=argparse.ArgumentParser(description="tuning script")
-#     parser.add_argument("--wandb_token", type=str, help="wandb token")
-#     args=parser.parse_args()
-#     WANDB_TOKEN=args.wandb_token
-#     study.optimize(
-#         objective,
-#         n_trials=100, 
-#         timeout=None
-#     )
-
-#     print("\n🏆 Best trial:")
-#     trial = study.best_trial
-
-#     print(f"  Accuracy: {trial.value:.4f}")
-#     print("  Params:")
-#     for k, v in trial.params.items():
-#         print(f"    {k}: {v}")
 import optuna
 import torch
 from pathlib import Path
 from main import train_detector 
 import argparse
-from modules.config import Config, FINAL_MAGNITUDE
-TRAIN_IMAGES = "/kaggle/input/bhw1/trainval"
-LABELS_CSV = "/kaggle/input/bhw1/labels.csv"
-SAVE_DIR = "optuna_models"
+from modules.config import Config
+from optuna.storages import JournalStorage
+from optuna.storages.journal import JournalFileBackend
+# TRAIN_IMAGES = "/kaggle/input/bhw1/trainval"
+# LABELS_CSV = "/kaggle/input/bhw1/labels.csv"
+# SAVE_DIR = "optuna_models"
+# WANDB_TOKEN=None
+# Path(SAVE_DIR).mkdir(exist_ok=True)
 WANDB_TOKEN=None
-Path(SAVE_DIR).mkdir(exist_ok=True)
+TRAIN_IMAGES = "bhw-1-dl-2025-2026/bhw1/trainval"
+LABELS_CSV = "bhw-1-dl-2025-2026/bhw1/labels.csv"
+SAVE_DIR = "optuna_models"
 def objective(trial: optuna.Trial):
     config = Config()
     config.LEARNING_RATE = trial.suggest_float(
         "learning_rate", 1e-5,1e-2, log=True
     )
     config.WEIGHT_DECAY = trial.suggest_float(
-        "learning_rate", 1e-5,1e-2, log=True
+        "weight_decay", 1e-5,1e-2, log=True
     )
-    FINAL_MAGNITUDE=trial.suggest_int(
+    config.MAGNITUDE=trial.suggest_int(
         "magnitude", 10, 70, step=10
     )
-    config.MAGNITUDE=FINAL_MAGNITUDE
     # size=trial.suggest_int(
     #     "window_size", 20, 60, step=4
     # )
@@ -126,29 +34,32 @@ def objective(trial: optuna.Trial):
     #     "model",
     #     ["RESNET18", "RESNET34", "RESNET50"]
     # )
+    config.MODEL="RESNET50"
     # config.MODEL="RESNET18"
     # config.NUM_BLOCKS=trial.suggest_int(
     #     "num_blocks",1, 5, step=1
     # )
-    # config.LAST_LINEAR_SIZE = trial.suggest_int(
-    #     "last_linear_size", 200, 1000, step=200
-    # )
-    # config.MARGIN_ARCFACE = trial.suggest_float(
-    #     "margin_arcface", 0.1,0.5
-    # )
+    config.LAST_LINEAR_SIZE = trial.suggest_int(
+        "last_linear_size", 200, 20000, step=200
+    )
+    config.MARGIN_ARCFACE = trial.suggest_float(
+        "margin_arcface", 0.0,0.5
+    )
 
-    # config.SCALE_ARCFACE = trial.suggest_int(
-    #     "scale_arcface", 8, 64, step=4
-    # )
+    config.SCALE_ARCFACE = trial.suggest_int(
+        "scale_arcface", 1, 10, step=1
+    )
     
     config.BATCH_SIZE = 2048
     config.SWA_START=None
     # config.LOSS = "ArcMargin"
     # config.OPTIMIZER="SGD"
-    config.NUM_EPOCHS = 30           
+    config.NUM_EPOCHS = 100
+    # config.STOP_EPOCH=10           
     config.WANDB_TOKEN = WANDB_TOKEN
-    # config.RUN_NAME=f"model_{config.MODEL}_Opt_{config.OPTIMIZER}_loss_{config.LOSS}_m_{config.MARGIN_ARCFACE:.2f}_s_{config.SCALE_ARCFACE}"
-    config.RUN_NAME=f"resnet50_ablation_lr_{config.LEARNING_RATE:.6f}_wd_{config.WEIGHT_DECAY:.6f}_m_{config.MAGNITUDE}"
+    config.RUN_NAME=f"model_{config.MODEL}_arcface_m_{config.MARGIN_ARCFACE:.2f}"
+    # config.RUN_NAME=f"resnet50_ablation_lr_{config.LEARNING_RATE:.6f}_wd_{config.WEIGHT_DECAY:.6f}_m_{config.MAGNITUDE}"
+    config.WANDB_PROJECT="Tuning Arcface"
     config.TRAININ_DIR=TRAIN_IMAGES
     try:
         best_acc = train_detector(
@@ -164,12 +75,13 @@ def objective(trial: optuna.Trial):
     return best_acc
 
 
-
 if __name__ == "__main__":
 
     study = optuna.create_study(
         direction="maximize",
-        study_name="arcface_model_search"
+        study_name="arcface_model_search",
+        storage=JournalStorage(JournalFileBackend(file_path="./journal.log")),
+        load_if_exists=True
     )
     parser=argparse.ArgumentParser(description="tuning script")
     parser.add_argument("--wandb_token", type=str, help="wandb token")
@@ -177,8 +89,9 @@ if __name__ == "__main__":
     WANDB_TOKEN=args.wandb_token
     study.optimize(
         objective,
-        n_trials=30, 
-        timeout=None
+        n_trials=100, 
+        timeout=None,
+        n_jobs=5
     )
 
     print("\n🏆 Best trial:")
--- modules/config.py
+++ modules/config.py
@@ -1,17 +1,58 @@
+# import os
+# import torch
+# class Config:
+#     WINDOW_SIZE:tuple[int, int]=(40, 40)
+#     LAST_LINEAR_SIZE:int=8400
+#     BATCH_SIZE:int=1024
+#     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     MEAN=None
+#     STD=None
+#     LEARNING_RATE:float=0.01
+#     ACCUM_STEP:int=1
+#     NUM_WORKERS:int=os.cpu_count() or 1
+#     LOG_STEP:int=5
+#     NUM_EPOCHS:int=3000
+#     LOSS:str="ArcMargin"
+#     MODEL:str="RESNET50"
+#     NUM_CLASSES:int=200
+#     MARGIN_ARCFACE:int=0.02477
+#     SCALE_ARCFACE:int=3
+#     WANDB_TOKEN:str=None
+#     WANDB_PROJECT:str="DL-BDZ-1_exp"
+#     RUN_NAME:str="resnet_18_arcface_m_10"
+#     OPTIMIZER:str="SGD"
+#     MOMENTUM:float = 0.9
+#     WEIGHT_DECAY:float=0.004603
+#     # NUM_BLOCKS:int=3
+#     # DROPOUT:float=0.5
+#     TRAININ_DIR:str|None=None
+#     DATAPARALLEL:bool=False
+#     PCT_START:float=0.05
+#     # SCHEDULER:str="CosineAnealing"
+#     SCHEDULER:str="OneCycle"
+#     STEPS_PER_EPOCH:int|None=None
+#     CLIP_GRAD_NORM:float=None
+#     SWA_START:int|None=2950
+#     SWA_LR:float|None=1e-4
+#     MAGNITUDE:int=60
+#     NUM_OPS_AUGS:int=1
+#     STOP_EPOCH:int=None
+
+#Resnet18
 import os
 import torch
 class Config:
     WINDOW_SIZE:tuple[int, int]=(40, 40)
-    LAST_LINEAR_SIZE:int=8400
+    LAST_LINEAR_SIZE:int=9200
     BATCH_SIZE:int=1024
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     MEAN=None
     STD=None
-    LEARNING_RATE:float=0.006615
+    LEARNING_RATE:float=0.00976
     ACCUM_STEP:int=1
     NUM_WORKERS:int=os.cpu_count() or 1
     LOG_STEP:int=5
-    NUM_EPOCHS:int=700
+    NUM_EPOCHS:int=3000
     LOSS:str="ArcMargin"
     MODEL:str="RESNET18"
     NUM_CLASSES:int=200
@@ -22,61 +63,20 @@ class Config:
     RUN_NAME:str="resnet_18_arcface_m_10"
     OPTIMIZER:str="SGD"
     MOMENTUM:float = 0.9
-    WEIGHT_DECAY:float=0.004603
+    WEIGHT_DECAY:float=0.007
     # NUM_BLOCKS:int=3
     # DROPOUT:float=0.5
     TRAININ_DIR:str|None=None
     DATAPARALLEL:bool=False
-    PCT_START:float=0.1
+    PCT_START:float=0.05
     # SCHEDULER:str="CosineAnealing"
     SCHEDULER:str="OneCycle"
     STEPS_PER_EPOCH:int|None=None
     CLIP_GRAD_NORM:float=None
-    SWA_START:int|None=650
-    SWA_LR:float|None=1e-2
+    SWA_START:int|None=2950
+    SWA_LR:float|None=1e-4
     MAGNITUDE:int=10
     NUM_OPS_AUGS:int=1
     STOP_EPOCH:int=None
 
 
-# import os
-# import torch
-# FINAL_MAGNITUDE=30
-# class Config:
-#     WINDOW_SIZE:tuple[int, int]=(40, 40)
-#     LAST_LINEAR_SIZE:int=3000
-#     BATCH_SIZE:int=1024
-#     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     MEAN=None
-#     STD=None
-#     LEARNING_RATE:float=4e-2
-#     ACCUM_STEP:int=1
-#     NUM_WORKERS:int=os.cpu_count() or 1
-#     LOG_STEP:int=5
-#     NUM_EPOCHS:int=450
-#     LOSS:str="CE"
-#     MODEL:str="RESNET50"
-#     NUM_CLASSES:int=200
-#     MARGIN_ARCFACE:int=0.20
-#     SCALE_ARCFACE:int=16
-#     WANDB_TOKEN:str=None
-#     WANDB_PROJECT:str="DL-BDZ-1_exp"
-#     RUN_NAME:str="resnet_50_lr_4e-2_with_swa_onecycle_sgd"
-#     OPTIMIZER:str="SGD"
-#     MOMENTUM:float = 0.9
-#     WEIGHT_DECAY:float=3e-3
-#     NUM_BLOCKS:int=3
-#     DROPOUT:float=0.5
-#     TRAININ_DIR:str|None=None
-#     DATAPARALLEL:bool=False
-#     PCT_START:float=0.1
-#     # SCHEDULER:str="CosineAnealing"
-#     SCHEDULER:str="OneCycle"
-#     STEPS_PER_EPOCH:int|None=None
-#     CLIP_GRAD_NORM:float=5.0
-#     SWA_START:int|None=350
-#     SWA_LR:float|None=1e-2
-#     MAGNITUDE:int=FINAL_MAGNITUDE
-#     NUM_OPS_AUGS:int=1
-#     STOP_EPOCH:int=None
-    
